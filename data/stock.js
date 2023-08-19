95228292221a01b27a1d85b2f0e5973f9eb0be11
Ezequiel Bernard <ezequielbernard2@gmail.com>
2023-08-19 18:39:28 -0300

    v0

---
+++ data/stock.js
@@ -0,0 +1,62 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Café Perú",
+        tipo: "café",
+        desc: "Un café exótico y lleno de sabor como su origen, dulce, con buen cuerpo y balanceado",
+        precio: 7500,
+        imagen: './public/images/peru.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Nueva Guinea",
+        tipo: "café",
+        desc: "Notas terrosas de caramelo, toques de canela y un dulzor suave a caramelo. Una breve escapada a la exótica isla en cada sorbo",
+        precio: 11500,
+        imagen: './public/images/papua.png',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Guatemala",
+        tipo: "café",
+        desc: "Una danza de sabores con matices a cacao intenso, notas de avellandas y un toque sutil de ciruela",
+        precio: 8900,
+        imagen: './public/images/guatemala1.png',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Café Bolivia",
+        tipo: "café",
+        desc: "Con notas florales a flor de azar y jazmín, una acidez característica del pacamara con notas a lima y frutos rojos y un final dulce predominante con notas a durazno y vainilla.",
+        precio: 12000,
+        imagen: './public/images/bolivia.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Café Colombia",
+        tipo: "café",
+        desc: "Es muy versátil, para todo tipo de métodos desde Espresso, Moka italiana, Prensa francesa hasta V60 y Chemex.",
+        precio: 4500,
+        imagen: './public/images/colombia2.png',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Café Nariño",
+        tipo: "café",
+        desc: "Este café nos presenta una taza delicada, con cuerpo bajo y notas florales a Jazmín y Earl gray, con un final dulce que nos recuerda al Durazno y la Miel. Dejando un retrogusto en boca prolongado.",
+        precio: 7500,
+        imagen: './public/images/colombia.jpg',
+        cantidad:1
+    }
+];
